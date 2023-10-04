4d099e8647af2dfb095c445ec071482ff8bb799f
SamNofee <sam@nofee.fun>
2023-10-05 01:30:06 +0800

    fix(kit): Change constructor

--- src/core/kit_base.ts
+++ src/core/kit_base.ts
@@ -1,12 +1,9 @@
-import { ApiBase } from './api_base'
 import { AppBase } from './app_base'
-import { CtxBase } from './ctx_base'
-import { ServiceBase } from './service_base'
 
-export class KitBase<O extends ServiceBase<ApiBase<AppBase, CtxBase>>> {
-  constructor(service: O) {
-    this.service = service
+export class KitBase<A extends AppBase> {
+  constructor(app: A) {
+    this.app = app
   }
 
-  public service: O
+  public app: A
 }
